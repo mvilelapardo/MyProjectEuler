065c2b5ee4d1bd44b12a420558f5875c18f8b3c5
Marcelo Vilela <mvilelapardo@gmail.com>
2017-02-05 17:05:48 -0400

    Solution, problem 1.

---
+++ 1to10/01-M35.cpp
@@ -0,0 +1,18 @@
+/*	Marcelo Vilela Pardo	*/
+
+#include "iostream"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+ int main(){
+ 	int sum = 0;
+ 	for (int i = 1; i < 1000; ++i){
+ 		if (i % 3 == 0 || i % 5 == 0){
+ 			sum += i;
+ 		}
+ 	}
+ 	cout << sum << endl;
+ 	return 0;
+ }
