2229383adc5c773aca3ed5aef0c182cb5a07f1fc
Marcelo Vilela <mvilelapardo@gmail.com>
2017-02-09 20:13:16 -0400

    Problem 8 with the text document with the big number

--- 1to10/08-LPIAS.cpp
+++ 1to10/08-LPIAS.cpp
@@ -15,8 +15,6 @@ int main(){
 
 	fscanf(fp, "%s", buffer);
 
-	//printf("%s\n", buffer);
-
 	for (int i = 0; i < 1000; ++i){
 		digitos[i] = buffer[i] - 48;
 	}
@@ -28,14 +26,12 @@ int main(){
 		for (int i = contador; i < contador + 13; ++i){
 			almacen *= digitos[i];
 		}
-		printf("%lu\n", almacen);
 		if (almacen > mayor){
 			mayor = almacen;
 		}
 		almacen = 1;
 		contador++;
 	}
-
 	printf("%lu\n", mayor);
 	fclose(fp);
 	return 0;
